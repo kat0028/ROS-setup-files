60a1596b72d9ba024ae69a562080f134620db627
kat0028 <85180845+kat0028@users.noreply.github.com>
2022-08-23 11:50:15 -0500

    Update noetic_20.sh

--- noetic_20.sh
+++ noetic_20.sh
@@ -4,7 +4,6 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
 sudo apt install ros-noetic-desktop-full -y
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
 sudo apt install python3-rosdep
