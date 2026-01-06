ea5b578704978bc1a604172019efaf794d6fe6c4
kat0028 <85180845+kat0028@users.noreply.github.com>
2026-01-06 14:09:50 -0600

    Add -y flag to apt commands for non-interactive installs
    Added '-y' flag to apt install and upgrade commands for automatic confirmation.

--- humble_22.sh
+++ humble_22.sh
@@ -7,7 +7,7 @@ export LANG=en_US.UTF-8
 
 locale  # verify settings
 
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository universe
 
 sudo apt update && sudo apt install curl -y
@@ -16,6 +16,6 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 
-sudo apt install ros-humble-desktop
+sudo apt install ros-humble-desktop -y
